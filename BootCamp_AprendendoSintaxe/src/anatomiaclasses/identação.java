0e0a3b942918bdd1127dda75e065c79041300fea
Pedro Silva <pedro.o.silva975@gmail.com>
2024-05-03 18:13:39 -0300

    new folder: Santander Bootcamp

---
+++ BootCamp_AprendendoSintaxe/src/anatomiaclasses/identação.java
@@ -0,0 +1,19 @@
+package anatomiaclasses;
+
+public class identação {
+
+	public static void main(String[] args) {
+		// Sem identação:
+		int nota = 5;
+		if (nota < 6)
+		System.out.println("Reprovado");
+		else if (nota == 6)
+		System.out.println("Recuperação");
+		else
+		System.out.println("Aprovado");
+	}
+	
+	
+	
+
+}
