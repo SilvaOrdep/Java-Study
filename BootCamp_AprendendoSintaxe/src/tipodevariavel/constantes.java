a8ff0278e66ad961b3a5ae6d162d2231e027bc1b
Pedro Silva <pedro.o.silva975@gmail.com>
2024-05-03 19:15:26 -0300

    modificação+adição de pastas das aulas do bootcamp

---
+++ BootCamp_AprendendoSintaxe/src/tipodevariavel/constantes.java
@@ -0,0 +1,10 @@
+package tipodevariavel;
+
+public class constantes {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ BootCamp_AprendendoSintaxe/src/tipodevariavel/variaveis.java
@@ -0,0 +1,10 @@
+package tipodevariavel;
+
+public class variaveis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
