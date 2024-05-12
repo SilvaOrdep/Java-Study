0ff89c4b299e99dfb2286a6aa05bf6090f52ada4
Pedro Silva <pedro.o.silva975@gmail.com>
2024-05-12 16:07:36 -0300

    update in Q10 - atividades.interacaouser

--- LivroAbordagemProgramçãoJava/src/atividades/interacaouser/Q10Paridade.java
+++ LivroAbordagemProgramçãoJava/src/atividades/interacaouser/Q10Paridade.java
@@ -1,5 +1,25 @@
 package atividades.interacaouser;
 
+import java.util.Scanner;
+
 public class Q10Paridade {
+	public static boolean paridade(int num) {
+		if (num%2==0) {
+			return  true;
+		} else {
+			return false;
+		}
+	}
 	
+	public static void main(String[] args) {
+		int num=0;
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Digite um número: true=par false=ímpar");
+		num = sc.nextInt();
+		
+		System.out.println(paridade(num));
+			
+		sc.close();
+	}
 }
