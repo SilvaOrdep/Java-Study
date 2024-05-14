9fec16d38bd64393895fac6cd62bc6d604c39bf4
Pedro Silva <pedro.o.silva975@gmail.com>
2024-05-13 22:05:21 -0300

    update

--- JavaBásico/src/vetores/Q05MaiorMenor.java
+++ JavaBásico/src/vetores/Q05MaiorMenor.java
@@ -9,10 +9,11 @@ public class Q05MaiorMenor {
 		
 		int v[] = new int[12];
 		
-		
 		System.out.println("Digite os valores do vetor:");
 		v[0] = sc.nextInt();
+		
 		int maior=0, menor = v[0];
+		
 		for (int i=1; i<v.length; i++) {
 			v[i] = sc.nextInt();
 			if (v[i] < menor) menor = v[i];
---
+++ JavaBásico/src/vetores/Q06Substituir.java
@@ -0,0 +1,26 @@
+package vetores;
+
+import java.util.Scanner;
+
+public class Q06Substituir {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int num = 0;
+		int vet[] = new int[10];
+		
+		System.out.println("Digite um valor para (num): ");
+		num = sc.nextInt();
+		
+		System.out.println("Digite os valores do array:");
+		for (int i = 0; i < vet.length; i++) {
+			vet[i] = sc.nextInt();
+		}
+		for (int i = 0; i < vet.length; i++) {
+			if (vet[i] == num) {
+				System.out.println(num+" foi encontrado no vetor, no índice "+i);
+			} 
+		}
+	}
+
+}
