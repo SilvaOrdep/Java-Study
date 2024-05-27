680e86c6109f47ed4d6a7021cbc4ecac6f2c2acd
Pedro Silva <pedro.o.silva975@gmail.com>
2024-05-27 20:33:15 -0300

    new file - fundPOO

---
+++ BootCamp_FundamentosPOO/src/classes/model/Student.java
@@ -0,0 +1,10 @@
+package classes.objetosInto;
+
+public class Student {
+    public String name;
+    public int age;
+
+    public void presentation(String name, int age) {
+        System.out.println("Hello my name is "+name+" and age is "+age);
+    }
+}
