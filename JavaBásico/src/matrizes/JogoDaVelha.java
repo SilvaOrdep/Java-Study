beec61ff6316c5dd69c7f8fd4ae69d2c9d16ddc0
Pedro Silva <pedro.o.silva975@gmail.com>
2024-06-07 21:31:37 -0300

    Jogo da velha feito em Java usando matrizes

--- JavaBásico/src/matrizes/JogoDaVelha.java
+++ JavaBásico/src/matrizes/JogoDaVelha.java
@@ -8,7 +8,7 @@ public class JogoDaVelha {
         final int l = 3;
         final int c = 3;
         boolean empate = false, vitoria = false;
-        int linha, coluna, jogar, opcao, cont = 0;
+        int linha, coluna, opcao, jogadas = 0, vez = 1;
         char[][] v = new char[l][c];
 
         do {
@@ -18,132 +18,194 @@ public class JogoDaVelha {
             switch (opcao) {
 
                 case 1:
-                    System.out.println("+-------------+");
-                    System.out.println("|Jogo da velha|");
-                    System.out.println("+_____+");
+                    vitoria = false;
+                    empate = false;
+                    jogadas = 0;
+                    System.out.println("+--------------------------------------------------------------------------------------------------+");
+                    System.out.println("\n" +
+                            "     ██  ██████   ██████   ██████      ██████   █████      ██    ██ ███████ ██      ██   ██  █████  \n" +
+                            "     ██ ██    ██ ██       ██    ██     ██   ██ ██   ██     ██    ██ ██      ██      ██   ██ ██   ██ \n" +
+                            "     ██ ██    ██ ██   ███ ██    ██     ██   ██ ███████     ██    ██ █████   ██      ███████ ███████ \n" +
+                            "██   ██ ██    ██ ██    ██ ██    ██     ██   ██ ██   ██      ██  ██  ██      ██      ██   ██ ██   ██ \n" +
+                            " █████   ██████   ██████   ██████      ██████  ██   ██       ████   ███████ ███████ ██   ██ ██   ██ \n");
+                    System.out.println("+--------------------------------------------------------------------------------------------------+");
                     System.out.println("               ");
 
-                    // Mostrar o jogo!!!!
+                    // MONTAR jogo:
+                    montarJogo(v, l, c);
 
-                    for (int i = 0; i < l; i++) {
-                        for (int j = 0; j < c; j++) {
-                            v[i][j] = ' ';
-                            System.out.printf("  |", v[i][j]);
-                        }
-                        System.out.println("");
-                    }
                     // VEZ DO X
                     do {
-                        System.out.println("Vez do X");
-                        System.out.println("Onde você quer jogar?");
-                        System.out.println("Digite a linha e a coluna:");
-                        linha = sc.nextInt();
-                        coluna = sc.nextInt();
-                        linha--;
-                        coluna--;
-                        //Verificando se a posição está ocupada
-                        if ((v[linha][coluna] == 'X') || (v[linha][coluna] == 'O')) {
-                            System.out.println("Essa posição ja está ocupada!");
-                            linha = 0;
-                            coluna = 0;
-                        }
-                        //Verificando se a posição digitada existe
-                        if ((linha >= 0 && linha < 3) && (coluna >= 0 && coluna < 3)) {
-                            v[linha][coluna] = 'X';
-
-                        } else {
-                            System.out.println("Digite uma posição valida!!!");
-                            break;
-                        }
-                        for (int i = 0; i < v.length; i++) {
-                            for (int j = 0; j < v.length; j++) {
-                                System.out.printf("%c |", v[i][j]);
+                        if (vez == 1) {
+                            System.out.println("Vez do X");
+                            System.out.println("Onde você quer jogar?");
+                            System.out.println("Digite a linha e a coluna:");
+                            linha = sc.nextInt();
+                            coluna = sc.nextInt();
+                            linha--;
+                            coluna--;
+                            // Verificando se a posição digitada existe
+                            if ((linha < 0 || linha > 2) && (coluna < 0 || coluna > 2)) {
+                                System.out.println("Digite uma posição valida!!!");
+                                continue;
                             }
-                            System.out.println("");
-                        } // Condição de vitoria X
-                        // Horizontal
-                        if ((v[0][0] == 'X' && v[0][1] == 'X' && v[0][2] == 'X')
-                                || (v[1][0] == 'X' && v[1][1] == 'X' && v[1][2] == 'X')
-                                || (v[2][0] == 'X' && v[2][1] == 'X' && v[2][2] == 'X')) {
-                            System.out.println(" VITORIA DO X");
-                            break;
-                        }
-                        // Vertical
-                        if (((v[0][0] == 'X' && v[1][0] == 'X' && v[2][0] == 'X')
-                                || (v[0][1] == 'X' && v[1][1] == 'X' && v[2][1] == 'X'))
-                                || (v[0][2] == 'X' && v[1][2] == 'X' && v[2][2] == 'X')) {
-                            System.out.println(" VITORIA DO X");
-                            vitoria = true;
-                            break;
-                        } // Diagonal
-                        if ((v[0][2] == 'X' && v[0][2] == 'X' && v[0][2] == 'X')
-                                || (v[0][2] == 'X' && v[0][2] == 'X' && v[0][2] == 'X')) {
-                            System.out.println(" VITORIA DO X");
-                            vitoria = true;
-                            break;
-
-                        }
-                        // Vez do O
-                        System.out.println("Vez do O");
-                        System.out.println("Onde você quer jogar?");
-                        System.out.println("Digite a linha e a coluna:");
-                        linha = sc.nextInt();
-                        coluna = sc.nextInt();
-                        if ((v[linha][coluna] == 'X') || (v[linha][coluna] == 'O')) {
-                            System.out.println("Essa posição ja está ocupada!");
-                            linha = 0;
-                            coluna = 0;
-                        }
-                        if ((linha >= 0 && linha < 3) && (coluna >= 0 && coluna < 3)) {
-                            v[linha][coluna] = 'O';
-
-                        } else {
-                            System.out.println("Digite uma posição valida!!!");
-                            break;
-                        }
-
-                        for (int i = 0; i < v.length; i++) {
-                            for (int j = 0; j < v.length; j++) {
-                                System.out.printf("%c |", v[i][j]);
+                            // Verificando se a posição está ocupada
+                            if ((v[linha][coluna] == 'X') || (v[linha][coluna] == 'O')) {
+                                System.out.println("Essa posição ja está ocupada!");
+                                linha = 0;
+                                coluna = 0;
+                                continue;
                             }
-                            System.out.println("");
-                        }
-                        // Condição de vitoria
+                            if ((linha >= 0 && linha < 3) && (coluna >= 0 && coluna < 3)) {
+                                v[linha][coluna] = 'X';
+                                vez = 0;
+                            }
 
-                        // Horizontal
-                        if ((v[0][0] == 'O' && v[0][1] == 'O' && v[0][2] == 'O')
-                                || (v[1][0] == 'O' && v[1][1] == 'O' && v[1][2] == 'O')
-                                || (v[2][0] == 'O' && v[2][1] == 'O' && v[2][2] == 'O')) {
-                            System.out.println(" VITORIA DO O");
-                            vitoria = true;
-                            break;
-                        } // Vertical
-                        if (((v[0][0] == 'O' && v[1][0] == 'O' && v[2][0] == 'O')
-                                || (v[0][1] == 'O' && v[1][1] == 'O' && v[2][1] == 'O'))
-                                || (v[0][2] == 'O' && v[1][2] == 'O' && v[2][2] == 'O')) {
-                            System.out.println(" VITORIA DO O");
-                            vitoria = true;
-                            break;
-                        } // Diagonal
-                        if ((v[0][2] == 'O' && v[0][2] == 'O' && v[0][2] == 'O')
-                                || (v[0][2] == 'O' && v[0][2] == 'O' && v[0][2] == 'O')) {
-                            System.out.println(" VITORIA DO O");
-                            vitoria = true;
-                            break;
+                            // Mostrar jogo:
+                            mostrarJogo(v, l, c);
+
+                            // Condição de vitoria X
+                            // Horizontal
+                            if ((v[0][0] == 'X' && v[0][1] == 'X' && v[0][2] == 'X')
+                                    || (v[1][0] == 'X' && v[1][1] == 'X' && v[1][2] == 'X')
+                                    || (v[2][0] == 'X' && v[2][1] == 'X' && v[2][2] == 'X')) {
+                                mostrarResultado(1);
+                                vitoria = true;
+                                break;
+                            }
+                            // Vertical
+                            if (((v[0][0] == 'X' && v[1][0] == 'X' && v[2][0] == 'X')
+                                    || (v[0][1] == 'X' && v[1][1] == 'X' && v[2][1] == 'X'))
+                                    || (v[0][2] == 'X' && v[1][2] == 'X' && v[2][2] == 'X')) {
+                                mostrarResultado(1);
+                                vitoria = true;
+                                break;
+                            } // Diagonal
+                            if ((v[0][0] == 'X' && v[1][1] == 'X' && v[2][2] == 'X')
+                                    || (v[0][2] == 'X' && v[1][1] == 'X' && v[2][0] == 'X')) {
+                                mostrarResultado(1);
+                                vitoria = true;
+                                break;
+
+                            }
+                            // Condição para empate
+                            jogadas++;
+                            if (jogadas == 9 && vitoria == false) {
+                                empate = true;
+                                mostrarResultado(2);
+                                break;
+                            }
+                        } else {
+                            // Vez do O
+                            System.out.println("Vez do O");
+                            System.out.println("Onde você quer jogar?");
+                            System.out.println("Digite a linha e a coluna:");
+                            linha = sc.nextInt();
+                            coluna = sc.nextInt();
+                            linha--;
+                            coluna--;
+                            // Verificando se a posição digitada existe
+                            if ((linha < 0 || linha > 2) && (coluna < 0 || coluna > 2)) {
+                                System.out.println("Digite uma posição valida!!!");
+                                continue;
+                            }
+                            // Verificando se a posição está ocupada
+                            if ((v[linha][coluna] == 'X') || (v[linha][coluna] == 'O')) {
+                                System.out.println("Essa posição ja está ocupada!");
+                                linha = 0;
+                                coluna = 0;
+                                continue;
+                            }
+                            //Colocando o O no espaço
+                            if ((linha >= 0 && linha < 3) && (coluna >= 0 && coluna < 3)) {
+                                v[linha][coluna] = 'O';
+                                vez = 1;
+                            }
+
+                            // Mostrar jogo:
+                            mostrarJogo(v, l, c);
+
+                            // Condição de vitoria
+                            // Horizontal
+                            if ((v[0][0] == 'O' && v[0][1] == 'O' && v[0][2] == 'O')
+                                    || (v[1][0] == 'O' && v[1][1] == 'O' && v[1][2] == 'O')
+                                    || (v[2][0] == 'O' && v[2][1] == 'O' && v[2][2] == 'O')) {
+                                mostrarResultado(0);
+                                vitoria = true;
+                                break;
+                            } // Vertical
+                            if (((v[0][0] == 'O' && v[1][0] == 'O' && v[2][0] == 'O')
+                                    || (v[0][1] == 'O' && v[1][1] == 'O' && v[2][1] == 'O'))
+                                    || (v[0][2] == 'O' && v[1][2] == 'O' && v[2][2] == 'O')) {
+                                mostrarResultado(0);
+                                vitoria = true;
+                                break;
+                            } // Diagonal
+                            if ((v[0][0] == 'O' && v[1][1] == 'O' && v[2][2] == 'O')
+                                    || (v[0][2] == 'O' && v[1][1] == 'O' && v[2][0] == 'O')) {
+                                mostrarResultado(0);
+                                vitoria = true;
+                                break;
+                            } // Condição para empate
+                            jogadas++;
+                            if (jogadas == 9 && vitoria == false) {
+                                empate = true;
+                                mostrarResultado(2);
+                                break;
+                            }
                         }
-                        if (vitoria = false) {
-                            empate = true;
-                        }
-                    } while (vitoria = false);
+                    } while (vitoria == false);
+                    vez = 1;
                     break;
                 case 2:
                     System.out.println("Para escolher a posição, utilize os numeros a seguir:");
-                    System.out.println("L=1,C=1|L=1,C=2|L=1,C=3" + "\nL=2,C=1|L=2,C=2|L=2,C=3" + "\nL=3,C=1|L=3,C=2|L=3,C=3");
+                    System.out
+                            .println("L=1,C=1|L=1,C=2|L=1,C=3" + "\nL=2,C=1|L=2,C=2|L=2,C=3" + "\nL=3,C=1|L=3,C=2|L=3,C=3");
                     break;
             }
         } while (opcao != 3);
     }
 
+    public static void mostrarResultado(int num){
+        if (num==1){
+            System.out.println("VITORIA DO X");
+        } else if (num==0){
+            System.out.println("VITORIA DO O");
+        } else if (num==2) {
+            System.out.println("\n" +
+                    "██    ██ ███████ ██      ██   ██  █████  \n" +
+                    "██    ██ ██      ██      ██   ██ ██   ██ \n" +
+                    "██    ██ █████   ██      ███████ ███████ \n" +
+                    " ██  ██  ██      ██      ██   ██ ██   ██ \n" +
+                    "  ████   ███████ ███████ ██   ██ ██   ██ \n" +
+                    "                                         \n");
+        }
+    }
+
+    public static void montarJogo(char[][] v, int l, int c){
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < c; j++) {
+                v[i][j] = ' ';
+                if (j == c - 1) {
+                    System.out.printf("%c", v[i][j]);
+                } else {
+                    System.out.printf("%c | ", v[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    public static void mostrarJogo(char[][] v, int l, int c){
+        for (int i = 0; i < l; i++) {
+            for (int j = 0; j < c; j++) {
+                if (j == c - 1) {
+                    System.out.printf("%c", v[i][j]);
+                } else {
+                    System.out.printf("%c | ", v[i][j]);
+                }
+            }
+            System.out.println();
+        }
+    }
 }
-
-
