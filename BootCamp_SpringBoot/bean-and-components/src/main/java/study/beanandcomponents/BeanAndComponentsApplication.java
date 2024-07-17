28fd5f3870f0386169b9a2f675b1be8245bca2c0
Pedro Silva <pedro.o.silva975@gmail.com>
2024-07-16 21:23:21 -0300

    Bean and Components

--- BootCamp_SpringBoot/bean-and-components/src/main/java/study/beanandcomponents/BeanAndComponentsApplication.java
+++ BootCamp_SpringBoot/bean-and-components/src/main/java/study/beanandcomponents/BeanAndComponentsApplication.java
@@ -1,7 +1,11 @@
 package study.beanandcomponents;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import study.beanandcomponents.app.ConversorJson;
+import study.beanandcomponents.app.ViaCepResponse;
 
 @SpringBootApplication
 public class BeanAndComponentsApplication {
@@ -10,4 +14,12 @@ public class BeanAndComponentsApplication {
 		SpringApplication.run(BeanAndComponentsApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner run(ConversorJson conversor) throws Exception {
+		return args -> {
+			String json = "{\"cep\": \"01001-000\",\"logradouro\": \"Praça da Sé\",\"localidade\": \"São Paulo\"}";
+			ViaCepResponse response = conversor.converter(json);
+			System.out.println("Dados do CEP: " + response);
+		};
+	}
 }
---
+++ BootCamp_SpringBoot/bean-and-components/src/main/java/study/beanandcomponents/app/Beans.java
@@ -0,0 +1,16 @@
+package study.beanandcomponents.app;
+
+
+import com.google.gson.Gson;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Beans {
+    @Bean
+    public Gson gson(){
+        return new Gson();
+    }
+
+
+}
