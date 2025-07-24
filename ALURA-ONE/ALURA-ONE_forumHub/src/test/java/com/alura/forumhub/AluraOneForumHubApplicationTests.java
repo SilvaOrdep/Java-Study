862f6bff998a5d0ff3c7bee0bb8a43a6c6dbe5bd
silvaOrdep <pedro.o.silva975@gmail.com>
2025-07-24 14:09:12 -0300

    build: novo projeto da alura

---
+++ ALURA-ONE/ALURA-ONE_forumHub/src/test/java/com/alura/forumhub/AluraOneForumHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alura.forumhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AluraOneForumHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
